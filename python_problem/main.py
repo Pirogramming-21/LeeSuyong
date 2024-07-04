983a9537a07afd0e0223306ef4b784b740e48806
leesuyong849 <toosee1005@cau.ac.kr>
2024-07-04 21:59:47 +0900

    stage 2

--- python_problem/main.py
+++ python_problem/main.py
@@ -1,2 +1,9 @@
 num = 0
 
+check = True
+while check :
+    userNum = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+    if userNum != 1 & userNum != 2 & userNum != 3:
+        continue
+    else:
+        check = False
