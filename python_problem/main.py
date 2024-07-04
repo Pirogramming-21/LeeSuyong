7a509a1f5d266b73fe7ab88324252fdd3e79d6d3
leesuyong849 <toosee1005@cau.ac.kr>
2024-07-04 22:29:27 +0900

    stage 5

--- python_problem/main.py
+++ python_problem/main.py
@@ -17,3 +17,19 @@ for i in range(userNum):
     num += 1
     print("playerA : " , num)
 
+check = True
+while check :
+    userNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+
+    try:
+        userNum = int(userNum)
+        if userNum == 1 or userNum == 2 or userNum == 3:
+            check = False
+        else:
+            print("1,2,3 중 하나를 입력하세요")
+    except:
+        print("정수를 입력하세요")
+
+for i in range(userNum):
+    num += 1
+    print("playerB : " , num)
